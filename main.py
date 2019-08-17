ff3c5cda4dcde5d5fc89acf2f0de55b525f021e0
mrx6SO <53888062+mrx6SO@users.noreply.github.com>
2019-08-17 04:54:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
     # The training set. We have 4 examples, each consisting of 3 input values
     # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_inputs = array([[3, 0, 1], [3, 1, 1], [1, 0, 3, ], [0, 3, 1]])
     training_set_outputs = array([[0, 1, 1, 0]]).T
 
     # Train the neural network using a training set.
@@ -70,5 +70,5 @@ if __name__ == "__main__":
     print neural_network.synaptic_weights
 
     # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-    print neural_network.think(array([1, 0, 0]))
+    print "Considering new situation [1, 0, 3] -> ?: "
+    print neural_network.think(array([1, 0, 3]))
